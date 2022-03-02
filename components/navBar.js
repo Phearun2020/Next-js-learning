fe76b452ed89d8f5a4bc60b803095820c84f23c1
Phearun2020 <phearun.ph77@gmail.com>
2022-03-02 11:45:08 +0700

    create-component

---
+++ components/navBar.js
@@ -0,0 +1,14 @@
+const NavBar = () => {
+    return (
+        <nav>
+            <div className="logo">
+                <h1>List of Product</h1>
+            </div>
+            <a>Home</a>
+            <a>About</a>
+            <a>Food</a>
+        </nav>
+    );
+}
+ 
+export default NavBar;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,16 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Footer from '../components/Footer'
+import NavBar from '../components/navBar'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div>
+      <NavBar />
       <h1>Home Page</h1>
+      <p>A home page is a webpage that serves as the starting point of website. It is the default webpage that loads when you visit a web address that only contains a domain name. For example, visiting https://techterms.com will display the Tech Terms home page. The home page is located in the root directory of a website.</p>
+      <Footer />
     </div>
   )
 }
