a2ca13cfc18ad4419c97efaa598d46d64c5d1abd
Phearun2020 <phearun.ph77@gmail.com>
2022-03-02 15:30:04 +0700

    add-styles-css

--- pages/index.js
+++ pages/index.js
@@ -8,10 +8,10 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div>
-      <h1>Home Page</h1>
-      <p>A home page is a webpage that serves as the starting point of website. It is the default webpage that loads when you visit a web address that only contains a domain name. For example, visiting https://techterms.com will display the Tech Terms home page. The home page is located in the root directory of a website.</p>
+      <h1 className={styles.title}>Home Page</h1>
+      <p className={styles.text}>A home page is a webpage that serves as the starting point of website. It is the default webpage that loads when you visit a web address that only contains a domain name. For example, visiting https://techterms.com will display the Tech Terms home page. The home page is located in the root directory of a website.</p>
       <Link href="/food">
-        <a>See lsit of food</a>
+        <a className={styles.btn}>See lsit of food</a>
       </Link>
     </div>
   )
